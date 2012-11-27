e42e2e74d018c5a55feaa847c20108420dea8dda
Maksym Polshcha <maxp@sterch.net>
2012-11-27 13:55:55 +0200

    0.2.5 fullnames parse can accept a schema as an argument. The schema defines an order of lastname, firstname, middlenames and suffix in a fullname.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(*rnames):
 
 setup(
     name='sterch.scrapingtools',
-    version='0.2.3',
+    version='0.2.5',
     url='http://pypi.sterch.net',
     license='ZPL',
     description='Library for building scrapers',
